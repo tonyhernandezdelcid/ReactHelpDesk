02a2cafcc612480bcd7e9f2c0382cf5a98b8ff12
tonyhernandezdelcid <tonyhernandezdelcid@gmail.com>
2023-06-22 16:48:42 -0600

    Se agregó la interfaz gráfica para Login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import SignIn from './componentes/login/SignIn';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Tony
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Tony
-        </a>
-      </header>
+
+      <SignIn></SignIn>
+
     </div>
   );
 }
